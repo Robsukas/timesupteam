5fba91b04987e06b466cde3aed2ec9b13e12d170
Robin Nook <ronook@ttu.ee>
2023-02-10 16:49:36 +0200

    Changed screen setting from main java class to specific classes, so from now on, every screen can be called out from specific screen files instead of clumping all the screen code in the main file.

--- client/core/src/com/timesupteam/TimesUpTeamGame.java
+++ client/core/src/com/timesupteam/TimesUpTeamGame.java
@@ -1,31 +1,23 @@
 package com.timesupteam;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.timesupteam.screens.PlayScreen;
 
-public class TimesUpTeamGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class TimesUpTeamGame extends Game {
+	public SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		super.render();
 	}
 }

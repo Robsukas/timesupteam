e21533ab8376aae5954455ebd0e686e1a7a90bd9
Robin Nook <ronook@ttu.ee>
2023-02-20 12:33:29 +0200

    Used FitViewport for now, so that the game can be played in full screen.

--- client/core/src/com/timesupteam/TimesUpTeamGame.java
+++ client/core/src/com/timesupteam/TimesUpTeamGame.java
@@ -8,8 +8,16 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.timesupteam.screens.PlayScreen;
 
 public class TimesUpTeamGame extends Game {
+
+	//Virtual width and height
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
+
+	//PPM - pixels per meter
+	public static final float PPM = 100;
+
 	public SpriteBatch batch;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();

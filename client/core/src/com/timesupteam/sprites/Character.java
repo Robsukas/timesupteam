e43abcb3570b187953ed7639437e30a50952d804
Robin Nook <ronook@ttu.ee>
2023-02-23 11:20:57 +0200

    character with movement added

---
+++ client/core/src/com/timesupteam/sprites/Character.java
@@ -0,0 +1,32 @@
+package com.timesupteam.sprites;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.*;
+import com.timesupteam.TimesUpTeamGame;
+
+public class Character extends Sprite {
+    public World world;
+    public Body b2Body;
+
+    private static final float playerDensity = 1.0f;
+
+    public Character(World world) {
+        this.world = world;
+        defineCharacter();
+    }
+
+    public void defineCharacter() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(32 / TimesUpTeamGame.PPM, 32 / TimesUpTeamGame.PPM);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        //bdef.fixedRotation = true;
+        b2Body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(3 / TimesUpTeamGame.PPM, 3 / TimesUpTeamGame.PPM);
+        fdef.shape = shape;
+        //fdef.density = playerDensity;
+        b2Body.createFixture(fdef);
+    }
+}

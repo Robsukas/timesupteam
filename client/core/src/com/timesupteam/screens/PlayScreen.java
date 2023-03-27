8e0cacdc7985d376bfc6e3b6503d7024a3707511
heohak <heohak@ttu.ee>
2023-03-27 12:39:51 +0300

    When timer reaches zero, game over screen pops up.

--- client/core/src/com/timesupteam/screens/PlayScreen.java
+++ client/core/src/com/timesupteam/screens/PlayScreen.java
@@ -232,7 +232,7 @@ public class PlayScreen implements Screen {
         hud.stage.draw();
 
         // Game over logic
-        if (false) {  // replace with real logic
+        if (hud.isTimeUp()) {  // replace with real logic
             game.setScreen(new GameOverScreen(game, "Y'all dead."));
             dispose();
         }
